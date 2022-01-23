1e145c8f2d906dc5267a34ceba7981f8220865a8
Joshua Boyd <jaboydfamily@gmail.com>
2022-01-23 11:13:53 -0500

    board now displays number choices for empty slots

--- board.rb
+++ board.rb
@@ -1,11 +1,18 @@
 class Board
   def initialize()
-    @board = Array.new(3, Array.new(3, ''))
+    @board = Array.new(3) {Array.new(3)}
   end
 
   public 
   def displayBoard()
-    @board.each do |row|
+    slot_counter = 1
+    @board.each_with_index do |row, rIndex|
+	  row.each_with_index do |slot, sIndex|
+	    if slot == nil
+		  @board[rIndex][sIndex] = slot_counter
+		  slot_counter += 1
+		end
+	  end
 	  p row
 	end
   end
